d6ef2d7283cda82773c67c0a39e9f7db2081a358
JM <earthxiaoyi@qq.com>
2016-06-11 13:34:52 +0800

    增加对象偏移量计算方法

--- src/main/java/leaderus/study8/UnsafeTest.java
+++ src/main/java/leaderus/study8/UnsafeTest.java
@@ -5,10 +5,23 @@ import java.lang.reflect.Field;
 import sun.misc.Unsafe;
 
 public class UnsafeTest {
+	
+	private static Unsafe unsafe = null;
+	
 	public static void main(String[] args) throws Exception{
 		Field f = Unsafe.class.getDeclaredField("theUnsafe");
 		f.setAccessible(true);
-		Unsafe unsafe = (Unsafe)f.get(null);
-		unsafe.allocateInstance(null);
+		unsafe = (Unsafe)f.get(null);
+	}
+	
+	/**
+	 * 获取对象在内存中的偏移量
+	 * @param obj
+	 * @return offset
+	 */
+	private static long getAddr(Object obj) {  
+	    Object[] array = new Object[]{obj};  
+	    long baseOffset = unsafe.arrayBaseOffset(Object[].class);  
+	    return unsafe.getLong(array, baseOffset);  
 	}
 }
