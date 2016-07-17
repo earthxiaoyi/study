62e2046edea4230db25ecf5dfc3abaab9f76c0e4
JM <earthxiaoyi@qq.com>
2016-07-17 12:51:14 +0800

    增加代码

--- src/main/java/leaderus/study8/RowDataOffheapStorage.java
+++ src/main/java/leaderus/study8/RowDataOffheapStorage.java
@@ -3,22 +3,27 @@ package leaderus.study8;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.concurrent.atomic.AtomicLong;
+
 
 //堆外存储，一次性使用，即记录保存完成后，读取完成后， 就关闭删除文件，不会修改数据
 public class RowDataOffheapStorage {
 	
 	private MappedByteBuffer buffer = null;
 	
+	private CloseMappedByteBuffer close = new CloseMappedByteBuffer();
+	
+	private AtomicLong heapOffset = new AtomicLong(0L);
+	
 	public RowDataOffheapStorage(String fileName) {
 		// 创建一个MappedByteBuffer 的映射文件存储，
 		File dataFile = new File(fileName);
 		try {
-			RandomAccessFile randomAccessFile = new RandomAccessFile(dataFile, "rw");
-			FileChannel fileChannel = randomAccessFile.getChannel();
-			buffer = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, Integer.MAX_VALUE);
+			RandomAccessFile accFile = new RandomAccessFile(dataFile, "rw");
+			FileChannel channel = accFile.getChannel();
+			buffer = channel.map(FileChannel.MapMode.READ_WRITE, 0, Integer.MAX_VALUE);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -26,11 +31,10 @@ public class RowDataOffheapStorage {
 
 	// 添加一行记录，返回此行的内存地址，用于RowData类记录
 	public long addRow(byte[] rowData) {
-		int position = buffer.position();
 		buffer.put(rowData);
-		return position;
+		return heapOffset.addAndGet(heapOffset.longValue()+rowData.length);
 	};
-
+	
 	// 读取相应位置的数据并返回
 	public byte[] getRow(long startPos, short dataLength) {
 		byte[] b = new byte[dataLength];
@@ -41,4 +45,14 @@ public class RowDataOffheapStorage {
 		}
 		return b;
 	};
+	
+	//删除映射文件
+	public void close(){
+		try {
+			close.clean(buffer);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- src/main/java/leaderus/study8/RowDataSorter.java
+++ src/main/java/leaderus/study8/RowDataSorter.java
@@ -1,6 +1,9 @@
 package leaderus.study8;
 
 public class RowDataSorter {
+	
+	private RowDataOffheapStorage storage = new RowDataOffheapStorage(null);
+	
 	// 添加一个新行，排序字段从第几个字节开始，长度是多少，比如一个字节，2个，4个，八个等，简单比较如下：按照ascii字符的大小排序，比如ab999<ba999
 	// 建议添加的过程中就拍好序（RowData序列）
 	public void addNewRow(byte[] rowData, int orderColStart, int orderColLen) {
@@ -12,5 +15,6 @@ public class RowDataSorter {
 
 	// 删除内存映射文件
 	public void close() {
+		storage.close();
 	}
 }
